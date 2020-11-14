47670ce74925369dc892b2def9c5341e1ca85eac
AzerKhamassi <azer.khamassi@gmail.com>
2020-11-14 21:02:50 +0100

    items fixes

--- api/controllers/orders.js
+++ api/controllers/orders.js
@@ -5,37 +5,42 @@ const User = require('../models/User');
 exports.createOrder = async (req, res, next) => {
   try {
     const user = await User.findById(req.user.userId);
-    if (!user) {
-      res
+    if (!user)
+      return res
         .status(404)
         .json({ message: 'The user with the given ID is not found' });
-    } else {
-      let items = [];
-      req.body.products.map(async (product) => {
-        let item = new Item({
-          product: product,
-          quantity: product.quantity,
+
+    new Promise((resolve, reject) => {
+      const items = [];
+      new Promise((r, j) => {
+        req.body.products.map(async (product) => {
+          const item = new Item({
+            product: product,
+            quantity: product.quantity,
+          });
+
+          item.save().then((res) => {
+            items.push(res);
+            r(res);
+          });
         });
-        let savedItem = await item.save();
-        // console.log(savedItem);
-        items.push(savedItem);
-      });
-      console.log(items);
-      let order = new Order({
-        items: items,
+      })
+        .then((res) => {
+          resolve(items);
+        })
+        .catch((err) => {
+          j(err);
+          reject(err);
+        });
+    }).then(async (resItems) => {
+      const order = new Order({
+        items: resItems.map((resItem) => resItem._id),
         user: req.user.userId,
         date: new Date().toISOString(),
       });
       await order.save();
-      res.status(201).json({ order: order });
-    }
-    // if (!product) {
-    //   res
-    //     .status(404)
-    //     .json({ message: 'The product with the given ID is not found' });
-    // } else {
-
-    // }
+      return res.status(201).json({ order: order });
+    });
   } catch (error) {
     res.status(500).json({ error: error });
   }
@@ -44,7 +49,7 @@ exports.createOrder = async (req, res, next) => {
 exports.getAllOrders = async (req, res, next) => {
   try {
     const orders = await Order.find()
-      // .populate('items')
+      .populate('items')
       .populate('items.products');
     // .populate({
     //   path: 'items.products',
