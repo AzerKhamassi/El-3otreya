86511a444641c36d403da3e67c8b6ae080d3d6c5
AzerKhamassi <azer.khamassi@gmail.com>
2021-01-01 18:11:22 +0100

    fixing port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const http = require('http');
 const app = require('./app');
-const port = process.env.PORT || 5000;
+const port = 8080;
 const server = http.createServer(app);
 
 server.listen(port);
